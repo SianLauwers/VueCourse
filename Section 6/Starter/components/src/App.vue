cd8486b6da9af39d8c8f632eb2ef3d4644a7b1ff
Sian Lauwers <r0889620@student.thomasmore.be>
2024-01-23 17:43:29 +0100

    Section 6 Part 1 - 7
    Sushi time

---
+++ Section 6/Starter/components/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Hey!</p>
+  <Greeting></Greeting>
+</template>
+
+<script>
+import Greeting from "@/components/Greeting.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Greeting,
+  },
+};
+</script>
