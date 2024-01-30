00f5350736914862c1b8ddea0cae78dd52a198f2
Sian Lauwers <r0889620@student.thomasmore.be>
2024-01-31 00:48:31 +0100

    idk

--- Section 10/Complete/music-10-18-COMPLETED/src/main.js
+++ Section 10/Complete/music-10-18-COMPLETED/src/main.js
@@ -1,15 +1,17 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import VeeValidatePlugin from './includes/validation';
-import './assets/tailwind.css';
-import './assets/main.css';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+import VeeValidatePlugin from "./includes/validation";
+
+import "./assets/base.css";
+import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(store);
+app.use(createPinia());
 app.use(router);
 app.use(VeeValidatePlugin);
 
-app.mount('#app');
+app.mount("#app");
